1ebd4e4d9a741a8b98bb48c6e8a672dbccd612f8
Skuffin <skuffin@gmx.de>
2016-05-27 19:00:46 +0200

    Added basic structure and test for constructed deck importing.

---
+++ HDTTests/DeckImport/DeckImportTest.cs
@@ -0,0 +1,26 @@
+﻿using Hearthstone_Deck_Tracker;
+using Hearthstone_Deck_Tracker.Utility;
+using Hearthstone_Deck_Tracker.Importing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDTTests.DeckImport
+{
+    [TestClass]
+    public class DeckImportTest
+    {
+        [TestMethod]
+        public void FullScreenCapture()
+        {
+            var bmp = new Bitmap("DeckImport/TestFiles/ScreenCaptureTest.bmp");
+
+            var deck = ConstructedDeckImporter.Import(bmp);
+        }
+
+    }
+}
---
+++ Hearthstone Deck Tracker/Importing/ConstructedDeckImporter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Hearthstone_Deck_Tracker.Hearthstone;
+
+namespace Hearthstone_Deck_Tracker.Importing
+{
+    public static class ConstructedDeckImporter
+    {
+        public static Deck Import(Bitmap bmp)
+        {
+
+
+            return null;
+        }
+    }
+}
